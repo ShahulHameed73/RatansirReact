962eb2fe8bd577c9d10a25910d599c48cfc5cbd4
ShahulHameed73 <mullashahull@gmail.com>
2024-11-02 14:44:45 +0530

    Update Blog.jsx

--- react-flow-app/src/Blog.jsx
+++ react-flow-app/src/Blog.jsx
@@ -4,6 +4,7 @@ function Blog() {
     
     return (
         <>
+            <h1>updated ur code</h1>
            <Post title="first component" body="first body" />
            <Post title="second component" body="second body" />
            <Post title="second component" body="third body" />
